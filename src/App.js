a5832787e30fca4b4256be64650776d9408dd93d
Francesco Agosti <francesco.agosti93@gmail.com>
2017-10-15 19:47:40 -0700

    Add linting

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
       this.setState({
         web3: results.web3
       })
-
       // Instantiate contract once web3 provided.
       this.instantiateContract()
     })
