ad4a1c9b2dc400e780a3d53171226f87c8a15da7
Francesco Agosti <francesco.agosti93@gmail.com>
2017-12-03 17:25:12 -0800

    Remove commented out contract code

--- contracts/EthereumCanvas.sol
+++ contracts/EthereumCanvas.sol
@@ -85,34 +85,6 @@ contract EthereumCanvas {
     }
     require(msg.value >= totalCost);
   }
-
-  // event PixelsChanged(
-  //   address owner,
-  //   uint[] xs,
-  //   uint[] ys,
-  //   bytes3[] colors
-  // );
- 
-  // function colorPixels(uint[] xs, uint[] ys, bytes3[] colors) payable {
-  //   require(xs.length == ys.length);
-  //   require(xs.length == colors.length);
-  //   require(msg.value >= xs.length*pixelPrice);
-  //   for (uint i = 0 ; i < xs.length ; i++) {
-  //     uint x = xs[i];
-  //     uint y = ys[i];
-  //     bytes3 color = colors[i];
-
-  //     Pixel storage pixel = pixels[x][y];
-
-  //     pixel.owner = msg.sender;
-  //     pixel.color = color;
-
-  //     if(pixel.owner != address(0)) {
-  //       pendingRefunds[pixel.owner] += pixelPrice;
-  //     }
-  //   }
-  //   PixelsChanged(msg.sender, xs, ys, colors);
-  // }
   
   // based on PullPayment.sol
   function withdrawRefunds() public {
